154056413d694420b1430100904d2a0d4a8adf67
3m1ss4ry <109291690+3m1ss4ry@users.noreply.github.com>
2023-09-08 14:42:15 +0000

    final commit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express()
 
-app.get("/endpoint", (req,res) => {
+app.get("/api", (req,res) => {
     const {slack_name, track} = req.query;
-    console.log(slack_name)
+    // console.log(slack_name)
     const current_day = new Date().toLocaleString('en-US', { weekday: 'long' });
-    const current_time_utc = new Date().toISOString();
+    const current_time_utc = new Date().toISOString().replace(/\.\d{3}Z$/, 'Z'); // Format UTC time
     
-    const github_file_url = ""
-    const github_repo_url = ""
+    const github_file_url = "https://github.com/olurocks/HNGX/blob/main/app.js"
+    const github_repo_url = "https://github.com/olurocks/HNGX.git"
 
     const response = {
         slack_name : slack_name,
